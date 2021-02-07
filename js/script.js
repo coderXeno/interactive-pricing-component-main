a4d9a5349647916ea5a82a58fb57d1bb5b0b89d3
Lucifer <unconfigured@null.spigotmc.org>
2021-02-07 20:02:29 +0200

    Project
    Final project

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let slider = document.querySelector("#slider");
+let price = document.querySelector(".number.price");
+let views = document.querySelector(".first.views");
+let pageviews = ["1 0 K", "5 0 K", "1 0 0 K", "5 0 0 K", "1 M"];
+let money = [8, 12, 16, 24, 36];
+views.innerHTML = `${pageviews[slider.value]} &nbsp; P A G E V I E W S`;
+price.innerHTML = `$${money[slider.value]}`;
+slider.oninput = function () {
+	views.innerHTML = `$ ${pageviews[this.value]} &nbsp; P A G E V I E W S`;
+	price.innerHTML = `$${money[slider.value]}`;
+};
+// let inter = null;
+// const setNumber = () => {
+// 	inter = setInterval(() => {
+// 		document.querySelector(".number").innerHTML =
+// 			"$" + Number(slider.value).toFixed(2);
+// 	}, 100);
+// };
+// slider.addEventListener("change", setNumber);
+// slider.addEventListener("mouseleave", () => {
+// 	clearInterval(inter);
+// });
+
+// setNumber();
